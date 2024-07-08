f44247b7a2058bb3bed48be86e15e59d44a3fb07
Sunil <v-2smanj@hotmail.com>
2024-07-08 19:28:02 +0530

    Remove-Merge pull request #1 from SunilManjhu/DebuggingBranch
    You are doomedddddd

--- io.cpp
+++ io.cpp
@@ -1,15 +1,1 @@
-#include "io.h"
-
-void writeAnswer(int)
-{
-    std::cout << "Hello lashya";
-}
-
-int readNumber()
-{
-    std::cout << "Enter a number: ";
-    int inputNumber{};
-    std::cin >> inputNumber;
-    return inputNumber;
-}
-
+DFAZDXDF
